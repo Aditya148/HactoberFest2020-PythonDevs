0a48868c3f0d6bf32908a6f0dbb1ff52efc951ba
Tanish Sarmah <70163670+tanishshadow@users.noreply.github.com>
2021-10-06 21:38:12 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,37 +1,27 @@
 num_1 = int(input('Enter your first number: '))
 num_2 = int(input('Enter your second number: '))
  
-# Addition
-print('{} + {} = '.format(num_1, num_2))
-print(num_1 + num_2)
- 
-# Subtraction
-print('{} - {} = '.format(num_1, num_2))
-print(num_1 - num_2)
- 
-# Multiplication
-print('{} * {} = '.format(num_1, num_2))
-print(num_1 * num_2)
- 
-# Division
-print('{} / {} = '.format(num_1, num_2))
-print(num_1 / num_2)
 
-num_1 = int(input('Enter your first number: '))
-num_2 = int(input('Enter your second number: '))
- 
+#  addition
+
 if choice == '+':
     print('{} + {} = '.format(num_1, num_2))
     print(num_1 + num_2)
+   
+ #subtraction
  
 elif choice == '-':
     print('{} - {} = '.format(num_1, num_2))
     print(num_1 - num_2)
  
+ #multiplication
+ 
 elif choice == '*':
     print('{} * {} = '.format(num_1, num_2))
     print(num_1 * num_2)
  
+#  division
+
 elif choice == '/':
     print('{} / {} = '.format(num_1, num_2))
     print(num_1 / num_2)
